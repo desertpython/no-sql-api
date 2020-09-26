8bf07cafdf45a55473a0e3d586a8f501ef594cf5
Desert Python <mattsenneff@aol.com>
2020-09-25 19:57:32 -0700

    first thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,30 @@
+
+const {Schema, model} = require('mongoose');
+const moment = require('moment');
+
+const ThoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        validate: function (thoughts) {
+            if(thoughts.length > 1 && thoughts.length < 280 ) {
+               return true;
+            } else {
+                return false;
+            }
+    
+        },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: {
+        // Array of nested documents created with the reactionSchema
+    }
+});
