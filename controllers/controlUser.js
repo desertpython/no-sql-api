9525f7f99e9fbb4d7f159fb18e935528deb57ef9
Desert Python <mattsenneff@aol.com>
2020-09-27 21:35:41 -0700

    friend controls

--- controllers/controlUser.js
+++ controllers/controlUser.js
@@ -82,6 +82,37 @@ const UserController = {
                 res.json(dbUserData);
             })
             .catch(err => res.status(400).json(err));
-    }
-}
+    },
+      
+    createFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  
+  removeFriend(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
+
+
+
 module.exports = UserController;
