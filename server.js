2e4dcbb268ed7571b04453db32123fee7c24c1e6
Desert Python <mattsenneff@aol.com>
2020-09-27 22:10:10 -0700

    added dsstore to gitignore

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.use(require('./routes/api'));
