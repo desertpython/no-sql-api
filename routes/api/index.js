414c27b61d23416030eb409386f8c3404544e663
Desert Python <mattsenneff@aol.com>
2020-09-27 22:17:15 -0700

    adjs to api code

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 // const thoughtsRoutes = require('./thoughts.js');
 const userRoutes = require('./user.js');
-
+const thoughtRoutes = require('./thoughts')
 
 // router.use('/thoughts', thoughtsRoutes);
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes)
 
-router.use((req, res) => {
-  res.status(404).send('<h1>😝 404 Error!</h1>');
-});
+// router.use((req, res) => {
+//   res.status(404).send('<h1>😝 404 Error!</h1>');
+// });
 
 module.exports = router;
