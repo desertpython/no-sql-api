b5fa71feead75569b444e3b05ab10ec97c6ac4c4
Desert Python <mattsenneff@aol.com>
2020-09-27 21:26:36 -0700

    reaction controls

--- controllers/controlThoughts.js
+++ controllers/controlThoughts.js
@@ -82,6 +82,47 @@ const thoughtController = {
                 res.json(dbThoughtData);
             })
             .catch(err => res.status(400).json(err));
-    }
-}
+    },
+    createReaction(req, res){
+        Thoughts.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+          )
+            .then((dbThoughtData) => {
+              if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No reaction with this id!' });
+              }
+              res.json(dbThoughtData);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+        },
+      
+    
+    deleteReaction(req,res) {
+        Thoughts.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+          )
+            .then((dbThoughtData) => {
+              if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No reaction with this id!' });
+              }
+              res.json(dbThoughtData);
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);
+            });
+        },
+    };
+      
+
+    
+
+
 module.exports = thoughtController;
